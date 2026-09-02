e5df8e59c1b3d7861dc20c8a19dcf340db274174
yujini <yujini1121@naver.com>
2026-09-03 03:38:41 +0900

    [Feature] 타이틀 게임 모드 선택 UI 추가

--- Bluff/Assets/_Game/Scripts/UI/TitleSceneController.cs
+++ Bluff/Assets/_Game/Scripts/UI/TitleSceneController.cs
@@ -4,33 +4,6 @@ using UnityEngine.SceneManagement;
 public sealed class TitleSceneController : MonoBehaviour
 {
     private const string GameplaySceneName = "Dev_Yujin";
-    private const float ButtonWidth = 260f;
-    private const float ButtonHeight = 52f;
-    private const float ButtonGap = 16f;
-
-    private void OnGUI()
-    {
-        float left = (Screen.width - ButtonWidth) * 0.5f;
-        float firstTop = Screen.height * 0.62f;
-
-        if (GUI.Button(
-                new Rect(left, firstTop, ButtonWidth, ButtonHeight),
-                "ROUND LIMITED"))
-        {
-            StartGame(GameMode.RoundLimited);
-        }
-
-        if (GUI.Button(
-                new Rect(
-                    left,
-                    firstTop + ButtonHeight + ButtonGap,
-                    ButtonWidth,
-                    ButtonHeight),
-                "ENDLESS"))
-        {
-            StartGame(GameMode.Endless);
-        }
-    }
 
     public void StartRoundLimited()
     {
